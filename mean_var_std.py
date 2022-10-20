89b86575c419cea0828693ed233a2028f5c8c597
Safa Ijaz <59706236+safaijaz808@users.noreply.github.com>
2022-10-20 19:14:04 +0000

    mean-variance-standard-deviation calculator

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,19 @@
+
+
 import numpy as np
 
 def calculate(list):
+    if len(list) < 9:
+        raise ValueError ('List must contain nine numbers.')
 
-
-
+    m = np.reshape(list, (3,3))
+    calculations = {
+        'mean': [np.mean(m, axis = 0).tolist(), np.mean(m, axis = 1).tolist(), np.mean(m).tolist()],
+        'variance': [np.var(m, axis = 0).tolist(), np.var(m, axis = 1).tolist(), np.var(m).tolist()],
+        'standard deviation': [np.std(m, axis = 0).tolist(), np.std(m, axis = 1).tolist(), np.std(m).tolist()],
+        'max': [np.max(m, axis = 0).tolist(), np.max(m, axis = 1).tolist(), np.max(m).tolist()],
+        'min': [np.min(m, axis = 0).tolist(), np.min(m, axis = 1).tolist(), np.min(m).tolist()],
+        'sum': [np.sum(m, axis = 0).tolist(), np.sum(m, axis = 1).tolist(), np.sum(m).tolist()]
+    }
 
     return calculations
